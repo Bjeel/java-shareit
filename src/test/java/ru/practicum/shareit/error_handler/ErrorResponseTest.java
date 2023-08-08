c2b96aba5039066c60fa3d7afef2b54e90e0e3ca
Bjeel <bjeel@yandex.ru>
2023-08-08 22:50:48 +0300

    remove wrong tests

--- src/test/java/ru/practicum/shareit/error_handler/ErrorResponseTest.java
+++ src/test/java/ru/practicum/shareit/error_handler/ErrorResponseTest.java
@@ -1,13 +1,10 @@
 package ru.practicum.shareit.error_handler;
 
 import org.junit.jupiter.api.Test;
-import ru.practicum.shareit.booking.Status;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ErrorResponseTest {
 
